86b4db789687814c43adee2436e0591587aaabad
mylesieong <myles.bcm@gmail.com>
2017-09-11 10:38:59 +0800

    Add launch4j to pom and enable build to windows exe

---
+++ src/main/java/com/bcm/app/Fo400.java
@@ -0,0 +1,12 @@
+package com.bcm.app;
+
+/**
+ * Class Fo400 is the command line class of fo400 project. It
+ * takes input parameters and launch jtopen utililies.
+ *
+ */
+public class Fo400 {
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
